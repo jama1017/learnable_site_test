94180b3b8fa7ee31820252615ead27271744186c
Jiaju Ma <majiaju@hotmail.com>
2018-01-10 21:30:49 -0500

    navbar updated in response to the new animation

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 $(function () {
   $(document).scroll(function () {
-    var $nav = $(".fixed-top");
+    var $nav = $(".navbar.navbar-expand-lg.navbar-dark.fixed-top");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
 
-anime({
-  targets: 'div.box.grey',
-  opacity: .5,
-  left: '40%',
-  translateY: '100%',
-  backgroundColor: '#FFF',
-  borderRadius: ['3em', '6em'],
-  easing: 'easeInOutQuad',
-  direction: 'alternate',
-  loop: true,
-});
+// anime({
+//   targets: 'div.box.grey',
+//   opacity: .5,
+//   left: '40%',
+//   translateY: '100%',
+//   backgroundColor: '#FFF',
+//   borderRadius: ['3em', '6em'],
+//   easing: 'easeInOutQuad',
+//   direction: 'alternate',
+//   loop: true,
+// });
