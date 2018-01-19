acea033251d4747cd8a5e0fdfeae3b18828942d2
Jiaju Ma <majiaju@hotmail.com>
2018-01-19 10:33:10 -0500

    layout branch merged back. New animation(v3.0) updated on site. Optimized style.css for future changes

--- js/main.js
+++ js/main.js
@@ -4,15 +4,3 @@ $(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
-
-// anime({
-//   targets: 'div.box.grey',
-//   opacity: .5,
-//   left: '40%',
-//   translateY: '100%',
-//   backgroundColor: '#FFF',
-//   borderRadius: ['3em', '6em'],
-//   easing: 'easeInOutQuad',
-//   direction: 'alternate',
-//   loop: true,
-// });
