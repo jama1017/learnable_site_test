c9e539a809ec72292ccfc72514571f04f2823895
Jiaju Ma <majiaju@hotmail.com>
2018-01-20 12:50:35 -0500

    Edited style.css to add hover effect on navbar links (change to theme blues), added scrollspy function. Enabled automatic navbar collapse after clicking on mobile platforms

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // changes navbar background, text, and hamburger color when scrolled
+  //changes navbar background, text, and hamburger color when scrolled
   $(function () {
     $(document).scroll(function () {
       var $nav = $(".navbar.navbar-expand-lg.navbar-dark.fixed-top");
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
   });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+ $('.nav-link').click(function() {
+   $('.navbar-collapse').collapse('hide');
+ });
 
 })(jQuery); // End of use strict
