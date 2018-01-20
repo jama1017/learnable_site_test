23b7447c42afb9bad95a89956a3354da037a6049
Jiaju Ma <majiaju@hotmail.com>
2018-01-19 21:48:26 -0500

    Moved navbar out the home-section in index.html. Strict mode added in main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-// changes navbar background, text, and hamburger color when scrolled
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar.navbar-expand-lg.navbar-dark.fixed-top");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+(function($) {
+  "use strict"; // Start of use strict
+
+  // changes navbar background, text, and hamburger color when scrolled
+  $(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar.navbar-expand-lg.navbar-dark.fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
   });
-});
+
+})(jQuery); // End of use strict
