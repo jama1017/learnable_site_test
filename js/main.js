de0c4073186eac02fcd279ed44d46668083fe115
Jiaju Ma <majiaju@hotmail.com>
2018-01-19 19:09:01 -0500

    Trying smooth scroll effect with css instead

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// changes navbar background, text, and hamburger color when scrolled
 $(function () {
   $(document).scroll(function () {
     var $nav = $(".navbar.navbar-expand-lg.navbar-dark.fixed-top");
